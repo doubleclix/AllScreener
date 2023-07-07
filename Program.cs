fbb44c607aeda84e49b3a278270942f3927c8deb
DESKTOP-0I5UNOD\Kyle <kepalmer@ymail.com>
2023-07-06 19:42:49 -0700

    Initial code to call into api, generated by chat-gpt.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
-﻿namespace AllScreener
+﻿using System;
+using System.IO;
+using System.Net;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string stockSymbol = "AAPL"; // Replace with the desired stock symbol
+
+        // URL for the Yahoo Finance API
+        string apiUrl = $"https://query1.finance.yahoo.com/v7/finance/options/{stockSymbol}";
+
+        // Create a WebClient instance to make the HTTP request
+        WebClient client = new WebClient();
+
+        try
         {
-            Console.WriteLine("Hello, World!");
+            // Download the option data
+            string jsonData = client.DownloadString(apiUrl);
+
+            Console.WriteLine(jsonData);
+
+            Console.WriteLine($"Option data for {stockSymbol} downloaded successfully.");
+        }
+        catch (WebException ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
 }
