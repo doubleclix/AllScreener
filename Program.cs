1618520c88152995ae5abb7dbe410749c8d0f456
DESKTOP-0I5UNOD\Kyle <kepalmer@ymail.com>
2023-07-06 19:55:22 -0700

    Added a method to get all of the expiration dates for a given symbol.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using Newtonsoft.Json.Linq;
 
 class Program
 {
@@ -8,24 +9,51 @@ class Program
     {
         string stockSymbol = "AAPL"; // Replace with the desired stock symbol
 
+        var expirationDates = GetExpirationDates(stockSymbol);
+
+        foreach (var expirationDate in expirationDates) 
+        { 
+            Console.WriteLine(expirationDate);
+        }
+    }
+
+
+    static DateTime[] GetExpirationDates(string symbol)
+    {
         // URL for the Yahoo Finance API
-        string apiUrl = $"https://query1.finance.yahoo.com/v7/finance/options/{stockSymbol}";
+        string apiUrl = $"https://query1.finance.yahoo.com/v7/finance/options/{symbol}";
 
         // Create a WebClient instance to make the HTTP request
         WebClient client = new WebClient();
 
         try
         {
-            // Download the option data
+            // Download the default, providing no date and get the option expirations
             string jsonData = client.DownloadString(apiUrl);
 
-            Console.WriteLine(jsonData);
+            JObject json = JObject.Parse(jsonData);
 
-            Console.WriteLine($"Option data for {stockSymbol} downloaded successfully.");
+            JToken optionChain = json["optionChain"]["result"][0];
+            JToken expirationDates = optionChain["expirationDates"];
+
+            // dates are stored as unix dates
+            var intDates = expirationDates.ToObject<int[]>();
+
+            List<DateTime> ret = new List<DateTime>();
+
+            foreach (int i in intDates)
+            {
+                ret.Add(DateTimeOffset.FromUnixTimeSeconds(i).Date);
+            }
+
+            return ret.ToArray();
         }
         catch (WebException ex)
         {
-            Console.WriteLine($"An error occurred: {ex.Message}");
+            Console.WriteLine($"An error occurred getting expiration dates. {ex.Message}");
+            throw;
         }
+
     }
+
 }
